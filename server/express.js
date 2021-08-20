b9b4e79238ba0f8c1e11fddc41b244ae3601cc33
Shokhrukh <turaevshokhrukh7@gmail.com>
2021-08-20 21:07:49 +0500

    ready it for production

--- server/express.js
+++ server/express.js
@@ -12,13 +12,13 @@ import lessonRoutes from './routes/lesson.routes'
 import commentRoutes from './routes/comment.routes'
 
 //comment out before building for production
-import devBundle from './devBundle'
+//import devBundle from './devBundle'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
 //comment out before building for production
-devBundle.compile(app)
+//devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
